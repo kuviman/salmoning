147aa0f1a1c4fd07680ebc48dd5b5e3944b75685
nertsal <nestralnertsal@gmail.com>
2024-04-15 21:52:26 +0300

    Skybox

--- assets/shaders/sky.glsl
+++ assets/shaders/sky.glsl
@@ -17,7 +17,7 @@ void main() {
 #ifdef FRAGMENT_SHADER
 
 uniform sampler2D u_gradient_texture;
-uniform sampler2D u_scribles_texture;
+uniform sampler2D u_scribbles_texture;
 
 void main() {
     mat4 inv = inverse(u_projection_matrix * u_view_matrix);
@@ -25,9 +25,14 @@ void main() {
     vec4 pos_b = inv * vec4(v_pos, 1.0, 1.0);
     vec3 dir = pos_b.xyz / pos_b.w - pos_a.xyz / pos_a.w;
 
-    float t = atan(dir.z / length(dir.xy)) / 3.14 + 0.5;
-    vec4 gradient_color = texture2D(u_gradient_texture, vec2(0.0, t));
+    float y = atan(dir.z / length(dir.xy)) / 3.14 + 0.5;
+    float x = atan(dir.x, dir.y) / 6.28 + 0.5;
 
-    gl_FragColor = gradient_color;
+    vec4 gradient_color = texture2D(u_gradient_texture, vec2(0.0, y));
+    float repeat = 4.0;
+    vec4 scribble_color = texture2D(u_scribbles_texture, vec2(x, y) * repeat);
+
+    vec3 color = gradient_color.rgb * (1.0 - scribble_color.a) + scribble_color.rgb * scribble_color.a;
+    gl_FragColor = vec4(color, 1.0);
 }
 #endif
