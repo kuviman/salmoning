0362cec03f168ec74bc6ecedb44bf8fa58f1c4b0
Sarah Schulte <git@sarah.engineer>
2024-04-14 23:27:09 -0700

    ez

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,17 +1,36 @@
 import "./style.css";
 
-(window as any).bridge_init = () => {
-  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+class Bridge {
+  app: Element;
+  money: Element;
+
+  constructor() {
+    document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1 id="money">$0</h1>
   </div>
+  <button>Hello</button>
 `;
-  const app = document.querySelector("#app")!;
-  const money = app.querySelector("#money")!;
+    this.app = document.querySelector("#app")!;
+    this.money = this.app?.querySelector("#money")!;
+  }
 
-  (window as any).bridge_sync_money = (amt: number) => {
-    money.innerHTML = `$${amt}`;
-  };
+  sync_money(amt: number): boolean {
+    this.money.innerHTML = `$${amt}`;
+    return true;
+  }
+}
+
+// the rest of this file is boilerplate; please ignore
+// thank you!
+
+let bridge: Bridge | undefined;
+(window as any).bridge_init = () => {
+  bridge = new Bridge();
 };
 
-
+Object.getOwnPropertyNames((Bridge as any).prototype).forEach((key) => {
+  (window as any)[`bridge_${key}`] = function () {
+    return bridge && (Bridge as any).prototype[key].apply(bridge, arguments);
+  };
+});
